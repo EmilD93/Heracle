085139ee0e48e508e2ab4010670d25d5f44bd9f7
User <teodorvlkov75@gmail.com>
2026-06-28 00:13:39 +0300

    Make action buttons always visible in OrganizerDashboard

--- src/components/OrganizerDashboard.tsx
+++ src/components/OrganizerDashboard.tsx
@@ -184,7 +184,7 @@ export function OrganizerDashboard({ setActiveTab, onDataChange, onEditEvent }: 
                         </div>
                       </td>
                       <td className="py-4 px-6 text-right">
-                        <div className="flex items-center justify-end gap-2 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity">
+                        <div className="flex items-center justify-end gap-2 transition-opacity">
                           {event.status === 'Draft' && (
                             <button
                               onClick={() => handlePublish(event.id)}
